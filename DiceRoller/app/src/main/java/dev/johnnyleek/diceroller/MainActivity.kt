577a079f28afff670c6ebd98df9fd674f1f12c22
Johnny Leek <jleek@esri.com>
2021-07-20 19:29:17 -0400

    Updated diceroller to use images

--- DiceRoller/app/src/main/java/dev/johnnyleek/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/dev/johnnyleek/diceroller/MainActivity.kt
@@ -3,10 +3,12 @@ package dev.johnnyleek.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,24 +18,51 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{ rollDice() }
 
         val countButton: Button = findViewById(R.id.count_button)
-        countButton.setOnClickListener { counter() }
+//        countButton.setOnClickListener { counter() }
+
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clearDice() }
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+//    private fun rollDiceText() {
+////        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
+//        val randomInt = (1..6).random()
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = randomInt.toString()
+//    }
+
+    private fun getRandomDiceImage(): Int {
+        return when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
-    private fun counter() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        if(resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else if(resultText.text == "6") {
-            resultText.text = "1"
-        } else {
-            resultText.text = (Integer.parseInt(resultText.text.toString()) + 1).toString()
-        }
+    private fun clearDice() {
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
     }
+
+//    private fun counter() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        if(resultText.text == "Hello World!") {
+//            resultText.text = "1"
+//        } else if(resultText.text == "6") {
+//            resultText.text = "1"
+//        } else {
+//            resultText.text = (Integer.parseInt(resultText.text.toString()) + 1).toString()
+//        }
+//    }
 }
